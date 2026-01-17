b2562b00943961e0be33e1491215f5eff4109f01
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2026-01-17 01:17:48 -0600

    feat: Import Sales, Earnings, and Sellers models in the init file

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,5 @@
 from app.models.user import User
 from app.models.product import Product
+from app.models.sales import Sales
+from app.models.earnings import Earnings
+from app.models.sellers import Sellers
